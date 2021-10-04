3af8ae5e3fda5d494ed219934d69716f18d69148
sinancengiz <sinancengiz_84@hotmail.com>
2021-10-04 16:02:58 -0700

    assets are creted and icons are added

---
+++ src/screen/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+const Landing = () => {
+  return (
+    <View>
+      <Text>Kog</Text>
+      <Text>Kingdom of Greatest</Text>
+    </View>
+  );
+};
+
+export default Landing;
