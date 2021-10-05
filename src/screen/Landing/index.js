e496a1a92223a2c1d8137ef73cd11294c6d232d1
sinancengiz <sinancengiz_84@hotmail.com>
2021-10-04 18:55:28 -0700

    styles are updated

--- src/screen/Landing/index.js
+++ src/screen/Landing/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {ScrollView, View, Text, Image, Pressable} from 'react-native';
+import {styles} from './Landing.component.style';
 
 const Landing = () => {
   return (
-    <View>
-      <Text>Kog</Text>
-      <Text>Kingdom of Greatest</Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Text style={styles.title}>KoG</Text>
+        <Text style={styles.subTitle}>Kingdom of The Greatest</Text>
+        <Image
+          style={styles.iconStyle}
+          source={require('../../assets/icons/castle(1).png')}
+        />
+        <Pressable
+          style={styles.loginButton}
+          onPress={() => {
+            alert('Login Button Pressed');
+          }}>
+          <Text style={styles.buttonText}>{'Login'}</Text>
+        </Pressable>
+        <Pressable
+          style={styles.signupButton}
+          onPress={() => {
+            alert('Signup Button Pressed');
+          }}>
+          <Text style={styles.buttonText}>{'Sign Up'}</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
   );
 };
 
