ed492884ce3b2c726f1c9c4be0288288e96fcb23
sinancengiz <sinancengiz_84@hotmail.com>
2021-10-04 19:53:25 -0700

    signup page and files are created

---
+++ src/screen/Signup/Signup.component.style.js
@@ -0,0 +1,52 @@
+import {Dimensions} from 'react-native';
+const {height} = Dimensions.get('window');
+
+export const styles = {
+  scrollView: {
+    backgroundColor: '#90EE90',
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#90EE90',
+    minHeight: height,
+  },
+  title: {
+    marginTop: '10%',
+    color: '#0000FF',
+    fontSize: 55,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif',
+  },
+  subTitle: {
+    marginTop: '5%',
+    color: '#0000FF',
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif',
+  },
+  iconStyle: {
+    width: 200,
+    height: 200,
+    marginTop: '10%',
+  },
+  loginButton: {
+    backgroundColor: 'green',
+    marginTop: '10%',
+    minWidth: 250,
+    minHeight: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 3,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  input: {
+    color: 'red',
+  },
+};
---
+++ src/screen/Signup/index.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Pressable,
+  TextInput,
+} from 'react-native';
+import {styles} from './Landing.component.style';
+
+const Login = () => {
+  const [email, onChangeEmail] = useState(null);
+  const [password, onChangePassword] = useState(null);
+  return (
+    <ScrollView style={styles.scrollView}>
+      <Text style={styles.title}>KoG</Text>
+      <Text style={styles.subTitle}>Kingdom of The Greatest</Text>
+      <View style={styles.container}>
+        <Image
+          style={styles.iconStyle}
+          source={require('../../assets/icons/castle(1).png')}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="Enter Email"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="Enter Password"
+        />
+        <Pressable
+          style={styles.loginButton}
+          onPress={() => {
+            alert('Login Button Pressed');
+          }}>
+          <Text style={styles.buttonText}>{'Login'}</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Login;
